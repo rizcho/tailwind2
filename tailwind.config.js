3126335f3cab17c3ec66726b4b15bfe6f499dbbe
Rizcho <adde.asryadi@gmail.com>
2022-02-07 21:30:56 +0700

    pertama commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'mel1' :'#011f2c'
+      },
+
+      spacing: {
+        '1280':'80rem',
+        '512':'32rem',
+        '97.5': '24.375rem',
+        '99.5': '24.875rem',
+        '98': '24.5rem'
+      }
+    },
+  },
+  plugins: [],
+}
